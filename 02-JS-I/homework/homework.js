dd21b634a8d1ffe0ce2968764d1ff66cff163c48
iLuka_ <elyapown2020@gmail.com>
2022-08-05 17:36:03 -0300

    48% Completed

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -7,7 +7,7 @@ const nuevaString = 'Wow';
 const nuevoNum = 20;
 
 // Crea una variable booleana:
-const nuevoBool = True;
+const nuevoBool = true;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - 5 === 5;
