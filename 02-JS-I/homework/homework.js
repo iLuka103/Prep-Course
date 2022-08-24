8130ebfecb277d95fd56d0e4c21019727546eb0c
iLuka_ <elyapown2020@gmail.com>
2022-08-24 16:16:25 -0300

    100%

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -217,12 +217,12 @@ function esVocal(letra){
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
   if (letra.length > 1) {
-    return "Dato incorrecto"
+    return "Dato incorrecto";
   }
   if(letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u"){
-    return "Es vocal"
+    return "Es vocal";
   }
-  return "Dato incorrecto"
+  return "Dato incorrecto";
 }
 
 
